0bb6c8c7e891c903e7d18b4d89573b15981acf28
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-12-09 12:39:56 +0100

    first commit

---
+++ one/one.js
@@ -0,0 +1,28 @@
+// Description: Find the elf with the most calories
+
+// import the input
+const input = require("./input.js");
+// input is defined as follows:
+// 1. Each group of numbers represents a single elf
+// 2. Each line(Number) represents the amount of calories of a single food item
+// 3. Each group is separated by a new line
+
+function findElfWithMaxCalories(input) {
+    // split the input into an array of elves (arrays of food items)
+    const elves = input.split("\n\n").map((group) => group.split("\n"));
+
+    // sum the calories of each elf
+    const elvesAsSums = elves.map((elf) => {
+        return elf.reduce((prev, curr) => +prev + +curr, 0);
+    });
+
+    // find the index of the max value
+    const maxIndex = elvesAsSums.findIndex(
+        (sum) => sum === Math.max(...elvesAsSums)
+    );
+
+    // return the max value
+    return elvesAsSums[maxIndex];
+}
+
+console.log(findElfWithMaxCalories(input));
